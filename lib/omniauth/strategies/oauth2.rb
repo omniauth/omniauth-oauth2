2da4e826c0f9ec1f597f4c221c1de347c8a9729d
Michael Bleigh <michael@intridea.com>
2011-10-01 23:59:25 -0500

    Symbolize options hash.

--- lib/omniauth/strategies/oauth2.rb
+++ lib/omniauth/strategies/oauth2.rb
@@ -62,7 +62,7 @@ module OmniAuth
 
       def build_access_token
         verifier = request.params['code']
-        client.auth_code.get_token(verifier, {:redirect_uri => callback_url}.merge(options.token_params))
+        client.auth_code.get_token(verifier, {:redirect_uri => callback_url}.merge(options.token_params.to_hash.inject({}){|h,(k,v)| h[k.to_sym] = v; h}))
       rescue ::OAuth2::Error => e
         raise e.response.inspect
       end
