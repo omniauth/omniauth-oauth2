ce64aaddb20724a1a5b7ff3707eb781fb5361221
Kevin Mehlbrech <kevin@kadenze.com>
2015-08-02 13:48:01 -0700

    updating new specs to use double quoted strings

--- spec/omniauth/strategies/oauth2_spec.rb
+++ spec/omniauth/strategies/oauth2_spec.rb
@@ -39,15 +39,15 @@ describe OmniAuth::Strategies::OAuth2 do
     end
 
     it "allows client_id to be passed as a lambda function" do
-      client_id = -> { 'abc' }
-      instance = subject.new(Object, client_id, 'def')
-      expect(instance.client.id).to eq('abc')
+      client_id = -> { "abc" }
+      instance = subject.new(Object, client_id, "def")
+      expect(instance.client.id).to eq("abc")
     end
 
     it "allows client_secret to be passed as a lambda function" do
-      client_secret = -> { 'def' }
-      instance = subject.new(Object, 'abc', client_secret)
-      expect(instance.client.secret).to eq('def')
+      client_secret = -> { "def" }
+      instance = subject.new(Object, "abc", client_secret)
+      expect(instance.client.secret).to eq("def")
     end
   end
 
