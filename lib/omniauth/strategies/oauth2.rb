738ce3d09916f1a8b23f378b0d3e5182a931bb3a
Pierre Pretorius <pierre@labs.epiuse.com>
2017-03-01 17:09:38 +0200

    Correctly redirect back to multiple origins after sign in.

--- lib/omniauth/strategies/oauth2.rb
+++ lib/omniauth/strategies/oauth2.rb
@@ -55,7 +55,11 @@ module OmniAuth
           @env ||= {}
           @env["rack.session"] ||= {}
         end
-        session["omniauth.state"] = params[:state]
+        session["omniauth.states"] ||= []
+        session["omniauth.states"] << params[:state]
+
+        session["omniauth.state_origins"] ||= {}
+        session["omniauth.state_origins"][params[:state]] = session['omniauth.origin']
         params
       end
 
@@ -63,15 +67,19 @@ module OmniAuth
         options.token_params.merge(options_for("token"))
       end
 
-      def callback_phase # rubocop:disable AbcSize, CyclomaticComplexity, MethodLength, PerceivedComplexity
+      def callback_phase
         error = request.params["error_reason"] || request.params["error"]
         if error
           fail!(error, CallbackError.new(request.params["error"], request.params["error_description"] || request.params["error_reason"], request.params["error_uri"]))
-        elsif !options.provider_ignores_state && (request.params["state"].to_s.empty? || request.params["state"] != session.delete("omniauth.state"))
+        elsif !options.provider_ignores_state && (request.params["state"].to_s.empty? || session["omniauth.states"].empty? || !session["omniauth.states"].include?(request.params["state"]))
           fail!(:csrf_detected, CallbackError.new(:csrf_detected, "CSRF detected"))
         else
           self.access_token = build_access_token
           self.access_token = access_token.refresh! if access_token.expired?
+
+          if session['omniauth.state_origins'] && session['omniauth.state_origins'][request.params['state']]
+            env['omniauth.origin'] = session['omniauth.state_origins'][request.params['state']]
+          end
           super
         end
       rescue ::OAuth2::Error, CallbackError => e
