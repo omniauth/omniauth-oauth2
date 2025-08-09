21f0dc051f90b31516c48ece0602bf0c2185d842
Mark James <mrj@advancedcontrols.com.au>
2025-08-09 20:04:19 +1000

    Add refresh_access_token & set_access_token_from_hash methods.

--- lib/omniauth/strategies/oauth2.rb
+++ lib/omniauth/strategies/oauth2.rb
@@ -89,7 +89,7 @@ module OmniAuth
           fail!(error, CallbackError.new(request.params["error"], request.params["error_description"] || request.params["error_reason"], request.params["error_uri"]))
         else
           self.access_token = build_access_token
-          self.access_token = access_token.refresh! if access_token.expired?
+          refresh_access_token if access_token.expired?
           super
         end
       rescue ::OAuth2::Error, CallbackError => e
@@ -98,6 +98,14 @@ module OmniAuth
         fail!(:timeout, e)
       rescue ::SocketError => e
         fail!(:failed_to_connect, e)
+      end
+
+      def set_access_token_from_hash(hash)
+        self.access_token = ::OAuth2::AccessToken.from_hash(client, hash)
+      end
+
+      def refresh_access_token
+        self.access_token = access_token&.refresh(token_params.to_hash(symbolize_keys: true), deep_symbolize(options.auth_token_params))
       end
 
     protected
